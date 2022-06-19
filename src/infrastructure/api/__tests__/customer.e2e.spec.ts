9482a108105d4434b97c872e3f71dde7cc84d936
Victor Severino <victor.hss@live.com>
2022-06-19 20:52:10 -0300

    Adding presenter to Customer

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -6,7 +6,7 @@ describe("E2E test for customer", () => {
     await sequelize.sync({ force: true })
   })
 
-  afterAll(async ()=> {
+  afterAll(async () => {
     await sequelize.close()
   })
 
@@ -20,7 +20,7 @@ describe("E2E test for customer", () => {
           city: "City 1",
           number: 123,
           zip: "Zip 1",
-        }
+        },
       })
 
     expect(response.status).toBe(200)
@@ -32,11 +32,9 @@ describe("E2E test for customer", () => {
   })
 
   it("should not create a customer", async () => {
-    const response = await request(app)
-      .post("/customer")
-      .send({
-        name: "User 1"
-      })
+    const response = await request(app).post("/customer").send({
+      name: "User 1",
+    })
 
     expect(response.status).toBe(500)
   })
@@ -51,7 +49,7 @@ describe("E2E test for customer", () => {
           city: "City 1",
           number: 123,
           zip: "Zip 1",
-        }
+        },
       })
     expect(response.status).toBe(200)
 
@@ -64,13 +62,11 @@ describe("E2E test for customer", () => {
           city: "City 2",
           number: 1234,
           zip: "Zip 2",
-        }
+        },
       })
     expect(response2.status).toBe(200)
 
-    const listResponse =  await request(app)
-      .get("/customer")
-      .send()
+    const listResponse = await request(app).get("/customer").send()
 
     expect(listResponse.status).toBe(200)
     expect(listResponse.body.customers.length).toBe(2)
@@ -80,5 +76,32 @@ describe("E2E test for customer", () => {
     const customer2 = listResponse.body.customers[1]
     expect(customer2.name).toBe("User 2")
     expect(customer2.address.street).toBe("Street 2")
+
+    const listResponseXML = await request(app)
+      .get("/customer")
+      .set("Accept", "application/xml")
+      .send()
+
+    expect(listResponseXML.status).toBe(200)
+    expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`)
+    expect(listResponseXML.text).toContain(`<customers>`)
+    expect(listResponseXML.text).toContain(`<customer>`)
+    expect(listResponseXML.text).toContain(`<name>User 1</name>`)
+    expect(listResponseXML.text).toContain(`<address>`)
+    expect(listResponseXML.text).toContain(`<street>Street 1</street>`)
+    expect(listResponseXML.text).toContain(`<city>City 1</city>`)
+    expect(listResponseXML.text).toContain(`<number>123</number>`)
+    expect(listResponseXML.text).toContain(`<zip>Zip 1</zip>`)
+    expect(listResponseXML.text).toContain(`</address>`)
+
+    expect(listResponseXML.text).toContain(`<customers>`)
+    expect(listResponseXML.text).toContain(`<customer>`)
+    expect(listResponseXML.text).toContain(`<name>User 2</name>`)
+    expect(listResponseXML.text).toContain(`<address>`)
+    expect(listResponseXML.text).toContain(`<street>Street 2</street>`)
+    expect(listResponseXML.text).toContain(`<city>City 2</city>`)
+    expect(listResponseXML.text).toContain(`<number>1234</number>`)
+    expect(listResponseXML.text).toContain(`<zip>Zip 2</zip>`)
+    expect(listResponseXML.text).toContain(`</address>`)
   })
 })
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express"
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase"
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase"
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository"
+import CustomerPresenter from "../presenters/customer.presenter"
 
 export const customerRoute = express.Router()
 
@@ -28,10 +29,9 @@ customerRoute.post("/", async (req: Request, res: Response) => {
 
 customerRoute.get("/", async (req: Request, res: Response) => {
   const usecase = new ListCustomerUseCase(new CustomerRepository())
-  try {
-    const output = await usecase.execute({})
-    res.send(output)
-  } catch (err) {
-    res.status(500).send()
-  }
+  const output = await usecase.execute({})
+  res.format({
+    json: async () => res.send(output),
+    xml: async () => res.send(CustomerPresenter.listXML(output)),
+  })
 })
